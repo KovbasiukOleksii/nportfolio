0d0edca0cc352504f083d162c757ebdbb0e65d3f
Kovbasiuk <kovbasiuk.oleksii@gmail.com>
2019-02-11 13:43:24 +0200

    new commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+ $(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+    
+new WOW().init();
+
+$('.slider').slick({
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1,
+      }
+    }
+  ]
+});
